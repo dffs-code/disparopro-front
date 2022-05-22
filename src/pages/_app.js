243221dc7ac8bc58d5a2c46925795cefc518af9e
Daniel <d.soares@aluno.ifsp.edu.br>
2022-05-21 22:59:00 -0300

    conecta com login e implementa react toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,22 @@
 import GlobalStyles from '../styles/global';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
+      <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          theme='dark'
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          draggable
+          pauseOnHover
+        />
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../public/logo.svg'
 import illustration from '../../public/illustration.svg'
 import styled from "styled-components";
-import Image from 'next/image';
+import api from '../services/api';
 import { FormContainer, FormItem, IconContainer, Button } from '../styles/formStyles';
 import { IllustrationContainer, FormWrapper } from '../styles/pageStyles';
 import { IoEye, IoEyeOff } from 'react-icons/io5'
-import Link from 'next/link';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -16,6 +18,24 @@ const Wrapper = styled.div`
 
 export default function Home() {
   const [ view, setView ] = useState(false);
+  const [firstParam, setFirstParam] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async () => {
+    if(!firstParam || !password) toast.warn('Preencha todos os campos')
+    
+    const payload = {
+      firstParam,
+      password
+    }
+
+    await api.post('/login', payload).then((response) => {
+      toast.success(`Usuário logado! Bem vindo(a), ${response.data.name}`)
+    }).catch((error) => {
+      toast.error(error.response.data.message)
+    })
+  }
+
   return (
     <Wrapper>
       <IllustrationContainer>
@@ -31,16 +51,16 @@ export default function Home() {
           <h1>Login</h1>
           <FormItem>
             <label>Email ou Celular</label>
-            <input type="text" id="login" className="input"/>
+            <input type="text" id="login" className="input" onChange={({target}) => setFirstParam(target.value)}/>
           </FormItem>
           <FormItem>
             <label>Senha</label>
-            <input type={view ? 'text' : 'password'} id="password" className="input"/>
+            <input type={view ? 'text' : 'password'} id="password" className="input" onChange={({target}) => setPassword(target.value)}/>
             <IconContainer onClick={() => setView(!view)}>
               {view ? <IoEyeOff /> : <IoEye />}
             </IconContainer>
           </FormItem>
-          <Button type="button" id="submit">Conectar</Button>
+          <Button type="button" id="submit" onClick={handleSubmit}>Conectar</Button>
           <span>Ainda não é cliente Disparo Pro?</span>
           <Link href='/register' className='create-account'>Criar Conta</Link>
         </FormContainer>
